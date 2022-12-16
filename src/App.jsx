a7b7f91636f8abddec21a86cb6e4e9841e36d831
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2022-12-16 17:36:12 -0300

    Add files src

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Welcome from "./components/Welcome/Welcome.jsx";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Quiz de Programação</h1>
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
---
+++ src/data/questions.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+        question: "O que é Vanilla JavaScript?",
+        options: [
+            "JavaScript puro",
+            "Uma biblioteca JavaScript",
+            "Um framework Javascript",
+            "Um compilador do JavaScript",
+        ],
+        answer: "JavaScript puro",
+    },
+    {
+        question: "Com qual instrução declaramos uma constante em JavaScript?",
+        options: [
+            "const",
+            "let",
+            "var",
+            "define",
+        ],
+        answer: "const",
+    },
+    {
+        question: "Qual o tipo de dado a seguir não existe em JavaScript?",
+        options: [
+            "string",
+            "number",
+            "boolean",
+            "float",
+        ],
+        answer: "float",
+    },
+    {
+        question: "Qual dos métodos a seguir seleciona um elemento?",
+        options: [
+            "querySelector",
+            "parseInt",
+            "sort",
+            "reduce",
+        ],
+        answer: "querySelector",
+    },
+    {
+        question: "Qual destas propriedades da a quantidade de elementos de um array?",
+        options: [
+            "qty",
+            "length",
+            "items",
+            "index",
+        ],
+        answer: "lenght",
+    },
+];
+
+export default data;
