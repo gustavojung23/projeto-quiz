4510e772fb824af28e608aeb25060ae8d5c383bc
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2022-12-20 11:57:44 -0300

    Add files is upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import  { useContext } from "react";
+import { QuizContext } from "./context/Quiz";
+
+import './App.css'
+import Welcome from "./components/Welcome/Welcome.jsx";
+import Questions from "./components/Questions/Questions";
+
+function App() {
+
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div className="App">
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Questions />}
+    </div>
+  )
+}
+
+export default App
