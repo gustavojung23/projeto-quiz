9f643ff71506f5913b4912df571fa4772c869de6
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2022-12-20 15:31:10 -0300

    Add files GameOver e Option

---
+++ src/components/Option/Option.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { QuizContext } from "../../context/Quiz";
+
+import "./Option.css";
+
+const Option = ({ option }) => {
+    
+    const [quizState, dispatch] = useContext(QuizContext);
+  
+    return (
+        <div className="option">
+            <p>{option}</p>
+        </div>
+    )
+}
+
+export default Option
