aa4e66df9210d0132cdb7f6658160f181364e5c9
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2022-12-20 15:30:30 -0300

    Update Questions.jsx / .css

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useContext } from 'react'
+import { QuizContext } from '../../context/Quiz';
+
+import Option from "../Option/Option";
+
+import './Questions.css';
 
 const Questions = () => {
-  return (
-    <div>Questions</div>
-  )
+
+    const [quizState, dispatch] = useContext(QuizContext);
+
+    const currentQuestion = quizState.questions[quizState.currentQuestion];
+
+    return (
+        <div id="question">
+            <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
+            <h2>{currentQuestion.question}</h2>
+            <div id="options-container">
+                {currentQuestion.options.map((option) => (
+                    <Option option={option} key={option} />
+                ))}
+            </div>
+            <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>Continuar</button>
+        </div>
+    )
 }
 
 export default Questions
