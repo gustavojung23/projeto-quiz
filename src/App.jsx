c29fb1e446ec27f13d791d13a6c7b4a2cf7a1eb7
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2022-12-20 15:27:09 -0300

    Update App.jsx / .css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import  { useContext } from "react";
+import  { useContext, useEffect } from "react";
 import { QuizContext } from "./context/Quiz";
 
-import './App.css'
 import Welcome from "./components/Welcome/Welcome.jsx";
 import Questions from "./components/Questions/Questions";
+import GameOver from "./components/GameOver/GameOver";
+
+import './App.css'
 
 function App() {
 
   const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    dispatch({ type: "REORDER_QUESTIONS" });
+  }, []);
 
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
       {quizState.gameStage === "Playing" && <Questions />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   )
 }
