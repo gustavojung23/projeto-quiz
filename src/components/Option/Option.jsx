ed1a7aae98163cd471454713b0dcb0403f9f289c
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2022-12-21 08:43:11 -0300

    Update Option

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -3,12 +3,17 @@ import { QuizContext } from "../../context/Quiz";
 
 import "./Option.css";
 
-const Option = ({ option }) => {
+const Option = ({ option, selectOption, answer }) => {
     
     const [quizState, dispatch] = useContext(QuizContext);
   
     return (
-        <div className="option">
+        <div className={`option ${
+                quizState.answerSelected && option === answer ? "correct" : ""
+            } ${
+                quizState.answerSelected && option !== answer ? "wrong" : ""}
+            `}
+            onClick={() => selectOption()}>
             <p>{option}</p>
         </div>
     )
